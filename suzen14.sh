7d76c686e1b5f4729bdf176b1c6732b2f00f4090
Serg-Den <den-serg2000@mail.ru>
2019-05-05 15:31:12 +0300

    used ls and cd

---
+++ suzen14.sh
@@ -0,0 +1,28 @@
+/home/suzen # ls
+diary.txt  part1
+/home/suzen # cd part1/
+/home/suzen/part1 # ls
+d2FodmFoM
+/home/suzen/part1 # cd ..
+/home/suzen # cd ..
+/home # ls
+john   suzen
+/home # cd john/
+/home/john # ls
+Desktop    Documents
+/home/john # cd Documents/
+/home/john/Documents # ls
+part2
+/home/john/Documents # cd part2/
+/home/john/Documents/part2 # ls
+WFlV2FpYm
+/home/john/Documents/part2 # cd ..
+/home/john/Documents # cd ..
+/home/john # cd Desktop/
+/home/john/Desktop # ls
+part3
+/home/john/Desktop # cd part3/
+/home/john/Desktop/part3 # ls
+VlaG9vMmIK
+/home/john/Desktop/part3 # 
+
