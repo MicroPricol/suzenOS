ec0d8c3719e460f0c9bf2c09d0f40b5831de25d8
Serg-Den <den-serg2000@mail.ru>
2019-05-05 17:56:06 +0300

    used rm and rm -rf

---
+++ suzen20.sh
@@ -0,0 +1,18 @@
+/home/suzen # cd ..
+/home # ls
+john   suzen
+/home # cd john/
+/home/john # ls
+Desktop    Documents  file1      file2      file3
+/home/john # rm -rf Desktop/
+/home/john # rm -rf Documents/
+/home/john # rm file1
+/home/john # rm file2
+/home/john # rm file3
+/home/john # cd ..
+/home # ls
+john   suzen
+/home # cd suzen/
+/home/suzen # ls
+YW1pZWhpaW0yb2h5NW9vRjZlaXcK
+
