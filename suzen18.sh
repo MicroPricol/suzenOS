74a7b4098d1c8f848a7b1260a9554ac66584f061
Serg-Den <den-serg2000@mail.ru>
2019-05-05 17:23:34 +0300

    Used mkdir

---
+++ suzen18.sh
@@ -0,0 +1,30 @@
+/home/suzen # man RTFM diary
+man: No entry for RTFM in the manual.
+man: outdated mandoc.db lacks diary(8) entry, run makewhatis /usr/local/man
+man(8)                          diary man page                          man(8)
+
+
+
+NAME
+       diary - little girls' diary
+
+DESCRIPTION
+       diary is a book in which one keeps a daily record of events and
+       experiences.
+
+OPTIONS
+       The diary does not take any options.
+
+FLAG
+       Y2hpZWNoM2VpRzRJZWtlaXNlbGUK
+
+BUGS
+       No known bugs.
+
+AUTHOR
+       bykva (@bykva)
+
+
+
+1.0                               20 Feb 2019                           man(8)
+
---
+++ suzen19.sh
@@ -0,0 +1,19 @@
+/home/suzen # mkdir Documents
+/home/suzen # cd Documents/
+/home/suzen/Documents # mkdir projects
+/home/suzen/Documents # cd projects/
+/home/suzen/Documents/projects # mkdir lab19
+/home/suzen/Documents/projects # cd lab19/
+/home/suzen/Documents/projects/lab19 # mkdir using
+/home/suzen/Documents/projects/lab19 # cd using/
+/home/suzen/Documents/projects/lab19/using # mkdir simple
+/home/suzen/Documents/projects/lab19/using # cd simple/
+/home/suzen/Documents/projects/lab19/using/simple # mkdir bash
+/home/suzen/Documents/projects/lab19/using/simple # cd bash/
+/home/suzen/Documents/projects/lab19/using/simple/bash # mkdir commands 
+/home/suzen/Documents/projects/lab19/using/simple/bash # cd commands/
+/home/suzen/Documents/projects/lab19/using/simple/bash/commands # ls
+/home/suzen/Documents/projects/lab19/using/simple/bash/commands # ls
+/home/suzen/Documents/projects/lab19/using/simple/bash/commands # ls
+V2VpMGNvaHNoZWlxdWE0YWhnaG8K
+
