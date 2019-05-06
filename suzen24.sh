b41f4b8e54ad2a218f8e56e92c6015172b549f20
Serg-Den <den-serg2000@mail.ru>
2019-05-06 14:33:30 +0300

    Used cp -r /..../* /../

---
+++ suzen24.sh
@@ -0,0 +1,6 @@
+/home/suzen # mkdir Music 
+/home/suzen # cp -r /home/suzen/Desktop/music/* /home/suzen/Music/
+/home/suzen # ls
+Desktop                       Music                         YWVnaG9venVvejd2b292OHNvaEwK
+
+
