a009bc8936edbbcb26b8a41784229af438b18cbd
Serg-Den <49840532+Serg-Den@users.noreply.github.com>
2019-05-25 12:33:04 +0300

    Update suzen19.sh

--- suzen19.sh
+++ suzen19.sh
@@ -1,19 +1,18 @@
-/home/suzen # mkdir Documents
-/home/suzen # cd Documents/
-/home/suzen/Documents # mkdir projects
-/home/suzen/Documents # cd projects/
-/home/suzen/Documents/projects # mkdir lab19
-/home/suzen/Documents/projects # cd lab19/
-/home/suzen/Documents/projects/lab19 # mkdir using
-/home/suzen/Documents/projects/lab19 # cd using/
-/home/suzen/Documents/projects/lab19/using # mkdir simple
-/home/suzen/Documents/projects/lab19/using # cd simple/
-/home/suzen/Documents/projects/lab19/using/simple # mkdir bash
-/home/suzen/Documents/projects/lab19/using/simple # cd bash/
-/home/suzen/Documents/projects/lab19/using/simple/bash # mkdir commands 
-/home/suzen/Documents/projects/lab19/using/simple/bash # cd commands/
-/home/suzen/Documents/projects/lab19/using/simple/bash/commands # ls
-/home/suzen/Documents/projects/lab19/using/simple/bash/commands # ls
-/home/suzen/Documents/projects/lab19/using/simple/bash/commands # ls
-V2VpMGNvaHNoZWlxdWE0YWhnaG8K
+#!/bin/bash
+mkdir Documents
+cd Documents/
+mkdir projects
+cd projects/
+mkdir lab19
+cd lab19/
+mkdir using
+cd using/
+mkdir simple
+cd simple/
+mkdir bash
+cd bash/
+mkdir commands 
+cd commands/
+ls
+
 
