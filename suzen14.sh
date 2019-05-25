dff733e17059f850d29ea20a36d7c335aa310b3a
Serg-Den <49840532+Serg-Den@users.noreply.github.com>
2019-05-25 12:24:33 +0300

    Update suzen14.sh

--- suzen14.sh
+++ suzen14.sh
@@ -1,28 +1,21 @@
-/home/suzen # ls
-diary.txt  part1
-/home/suzen # cd part1/
-/home/suzen/part1 # ls
-d2FodmFoM
-/home/suzen/part1 # cd ..
-/home/suzen # cd ..
-/home # ls
-john   suzen
-/home # cd john/
-/home/john # ls
-Desktop    Documents
-/home/john # cd Documents/
-/home/john/Documents # ls
-part2
-/home/john/Documents # cd part2/
-/home/john/Documents/part2 # ls
-WFlV2FpYm
-/home/john/Documents/part2 # cd ..
-/home/john/Documents # cd ..
-/home/john # cd Desktop/
-/home/john/Desktop # ls
-part3
-/home/john/Desktop # cd part3/
-/home/john/Desktop/part3 # ls
-VlaG9vMmIK
-/home/john/Desktop/part3 # 
+#!/bin/bash 
+ls
+cd part1
+ls
+cd ..
+cd ..
+ls
+cd john
+ls
+cd Documents
+ls
+cd part2
+ls
+cd ..
+cd ..
+cd Desktop/
+ls
+cd part3/
+ls
+
 
